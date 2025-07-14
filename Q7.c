7002efc8987062e0a0458f7f24dacbc0ba36b3bf
Yamada731 <hinataakunn98@gmail.com>
2025-07-14 23:35:16 +0530

    Check Whether a Year is Leap Year or Not
    Check Whether a Year is Leap Year or Not

--- Q7.c
+++ Q7.c
@@ -3,14 +3,15 @@
 int main() {
     int year;
 
+    // User pora year loi ase
     printf("Enter a year: ");
     scanf("%d", &year);
 
-    // leap year or logic
+    // Leap year rule check kore
     if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0))
-        printf("%d is a leap year.\n", year);
+        printf("%d is a Leap Year\n", year);
     else
-        printf("%d is not a leap year.\n", year);
+        printf("%d is not a Leap Year\n", year);
 
     return 0;
 }
